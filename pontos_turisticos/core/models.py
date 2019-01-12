0197144619074b652671465a535f53fe73bbb210
Pedro Marques <junnior.pedro.pm@gmail.com>
2019-01-12 18:28:51 -0300

    relating Pontos turisticos with Atracoes using a m2m relationship

--- pontos_turisticos/core/models.py
+++ pontos_turisticos/core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from atracoes.models import Atracao
 
 class PontoTuristico(models.Model):
     #dados básicos
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+
+    #lista de atrações
+    atracoes = models.ManyToManyField(Atracao)
 
     def __str__(self):
         return self.nome
--- pontos_turisticos/pontos_turisticos/settings.py
+++ pontos_turisticos/pontos_turisticos/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-    'pontos_turisticos',
+    'atracoes',
 ]
 
 MIDDLEWARE = [
