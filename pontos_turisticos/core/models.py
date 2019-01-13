530e87973ee0855e5a3e1b62d2e056f7d1126006
Pedro Marques <junnior.pedro.pm@gmail.com>
2019-01-12 23:09:11 -0300

    added localization to turistics points

--- pontos_turisticos/core/models.py
+++ pontos_turisticos/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
 
 class PontoTuristico(models.Model):
     #dados básicos
@@ -12,10 +13,16 @@ class PontoTuristico(models.Model):
     #lista de atrações
     atracoes = models.ManyToManyField(Atracao)
 
-    #comentarios sobre essa atração
+    #comentarios sobre esse ponto turistico
     comentarios = models.ManyToManyField(Comentario)
 
+    #avaliações sobre esse ponto turistico
     avaliacoes = models.ManyToManyField(Avaliacao)
+
+    #endereço do ponto turistico
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
+
 
     def __str__(self):
         return self.nome
